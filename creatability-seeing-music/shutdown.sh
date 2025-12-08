21efbe003e96326f587b755446f29ab631066c59
Shine <850429007@qq.com>
2025-12-08 14:28:49 +0800

    更新启停脚本

--- creatability-seeing-music/shutdown.sh
+++ creatability-seeing-music/shutdown.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-pid=$(ps -ef|grep 'npm run build && http-server -p 8106' | grep -v grep | awk '{print $2}')
+pid=$(cat ./run.pid)
 if [ -n "$pid" ]; then
     echo "Killing process with PID $pid"
-    kill -9 "$pid"
+    kill -- -$(ps -o pgid= -p ${pid} | tr -d ' ')
 else
     echo "No process found with the specified command"
 fi
--- creatability-seeing-music/start.sh
+++ creatability-seeing-music/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup npm run start &
+nohup npm run start & echo $! > run.pid
