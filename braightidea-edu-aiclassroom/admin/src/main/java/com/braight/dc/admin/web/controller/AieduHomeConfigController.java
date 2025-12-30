e38cfc93492153963a4e3a008bd656e91577bf5c
Shine <850429007@qq.com>
2025-12-30 16:39:43 +0800

    新增 首页配置 菜单

---
+++ braightidea-edu-aiclassroom/admin/src/main/java/com/braight/dc/admin/web/controller/AieduHomeConfigController.java
@@ -0,0 +1,43 @@
+package com.braight.dc.admin.web.controller;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.braight.dc.admin.web.entity.AieduHomeConfigPO;
+import com.braight.dc.admin.web.mapper.AieduHomeConfigPOMapper;
+import com.braight.master.common.core.controller.BaseController;
+import com.braight.master.common.core.domain.AjaxResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+/**
+ * 首页配置
+ *
+ * @author Shine
+ * @date 2025/12/30
+ */
+@RestController
+public class AieduHomeConfigController extends BaseController {
+    @Resource
+    private AieduHomeConfigPOMapper aieduHomeConfigPOMapper;
+
+
+    @GetMapping("/api/aieduHomeConfig/{id}")
+    public AjaxResult getById(@PathVariable Integer id) {
+        AieduHomeConfigPO aieduHomeConfigPO = aieduHomeConfigPOMapper.selectByPrimaryKey(id);
+        return success(aieduHomeConfigPO);
+    }
+
+    @PostMapping("/api/aieduHomeConfig/edit")
+    public AjaxResult edit(@RequestBody AieduHomeConfigPO aieduHomeConfigPO) {
+        JSONObject solutionDetail = aieduHomeConfigPO.getHomeConfig();
+        if (solutionDetail != null) {
+            aieduHomeConfigPO.setContentJson(JSON.toJSONString(solutionDetail));
+        } else {
+            aieduHomeConfigPO.setContentJson("");
+        }
+        int i = aieduHomeConfigPOMapper.updateByPrimaryKey(aieduHomeConfigPO);
+        return i > 0 ? success() : error();
+    }
+
+}
